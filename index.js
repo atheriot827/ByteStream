0e74b0411b18edd8b7fde7d484a8ebfa3487495f
Ashley Theriot <atheriot827@gmail.com>
2024-10-02 11:00:08 -0500

    add notes

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 
+//ALL CODE GOES INSDIE HERE 
 $(document).ready(() => {
   const $body = $('body');
-  $body.html(''); //clears body
+  $body.html(''); //clears body - will clear tag you call it on
 
+  //create a tweets div
+
+  //put this into a function
+  function createTweets() {
   const $tweets = streams.home.map((tweet) => {
     const $tweet = $('<div></div>');
     const text = `@${tweet.user}: ${tweet.message}`;
@@ -12,5 +17,16 @@ $(document).ready(() => {
     return $tweet;
   });
   $body.append($tweets);
-
+}
 });
+
+//streams.home is an array of tweets
+//console.log streams.home -> constantly new tweets(?) are being added to streams.home because of data-generator.js
+//will need the actual time (moment - format dates) and the human friendly time (moment - relative time)
+  //google cdn moment -> we're using jQuery so add script tag to head of html file and paste in link from the google search of cdn moment
+//everything should be done in jQuery even styling
+//all tweets are being put into body - suggests having tweets go into a separate div (instead of appending to body, add a tweets div)
+//inside function createTweets():
+  //tweet.user will need to be clickable -> out in own tag
+  //add timestamp
+  //const test will change
